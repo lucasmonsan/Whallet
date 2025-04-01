1325fe91bfca209b86a9dcc3fe389894c0469751
lucasmonsan <lucasmonsan@gmail.com>
2025-04-01 15:08:38 -0300

    Organização de OLDs

--- OLD_1/src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Footer from '../components/layout/Footer.svelte';
-	import Header from '../components/layout/Header.svelte';
-	import '../styles/global.css';
-</script>
-
-<Header />
-<slot />
-<Footer />
